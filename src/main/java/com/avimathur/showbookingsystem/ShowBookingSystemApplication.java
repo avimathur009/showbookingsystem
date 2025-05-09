e37a0d22914abd42479face4b61fab6551aa4cb3
Avi Mathur <avi.mathur009@gmail.com>
2025-05-09 08:40:43 +0530

    This is a part of my submission of my mock Java project for Thinkify Organization.

---
+++ src/main/java/com/avimathur/showbookingsystem/ShowBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.avimathur.showbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowBookingSystemApplication.class, args);
+	}
+
+}
