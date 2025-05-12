ea913bd0629fdf6c2adac37f6463ed647351a61a
Avi Mathur <avi.mathur009@gmail.com>
2025-05-12 19:16:18 +0530

    Removed RnkingStrategy.java as a Bean

--- src/main/java/com/avimathur/showbookingsystem/service/RankingStrategy.java
+++ src/main/java/com/avimathur/showbookingsystem/service/RankingStrategy.java
@@ -2,12 +2,10 @@ package com.avimathur.showbookingsystem.service;
 
 import com.avimathur.showbookingsystem.constant.ShowType;
 import com.avimathur.showbookingsystem.pojo.LiveShow;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Map;
 
-@Component
 public abstract class RankingStrategy {
 
     public abstract void displayAllShows(Map<String, ArrayList<LiveShow>> listOfShows);
